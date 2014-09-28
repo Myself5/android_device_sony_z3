3d6d5c9263dae6b3ded92f56f69827c495b7be8d
Zyg0te <edvard.holst@gmail.com>
2014-09-28 19:03:25 +0200

    Corrected device name reference in cm.mk

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/sony/sirius/full_leo.mk)
+$(call inherit-product, device/sony/leo/full_leo.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603 BUILD_FINGERPRINT=Sony/D6603/D6603:4.4.2/23.0.A.2.93/xf5vdw:user/release-keys PRIVATE_BUILD_DESC="D6603-user 4.4.2 23.0.A.2.93 xf5vdw release-keys"
 
